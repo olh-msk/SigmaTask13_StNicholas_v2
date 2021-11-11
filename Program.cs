b010a8059dcf2a9fbdec3266ab61d9b7222425e6
Oleh <olh.masiuk@gmail.com>
2021-11-11 22:37:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SigmaTask13_StNicholas_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
